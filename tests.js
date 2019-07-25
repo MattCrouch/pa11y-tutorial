89ace9153f884dc92671d07e7535a4e1bc219168
Matt Crouch <matt@mattcrouch.net>
2019-07-25 22:21:13 +0100

    Rough first cut

--- tests.js
+++ tests.js
@@ -1,1 +1,19 @@
-console.log("HEEEY");
+const pa11y = require("pa11y");
+
+pa11y("localhost:8080", {
+  includeWarnings: true,
+  standard: "WCAG2AA",
+  level: "notice",
+  log: {
+    debug: console.log,
+    error: console.error,
+    info: console.info
+  },
+  screenCapture: `${__dirname}/my-screen-capture.png`
+})
+  .then(results => {
+    console.log(results);
+  })
+  .catch(e => {
+    console.log(e);
+  });
