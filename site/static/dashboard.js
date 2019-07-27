edfee8a05722628cd6d5bb9d6ff1eebae4881e49
Matt Crouch <matt@mattcrouch.net>
2019-07-27 19:46:00 +0100

    Remove logs

--- site/static/dashboard.js
+++ site/static/dashboard.js
@@ -1,11 +1,9 @@
 (function() {
   const issueLink = document.querySelectorAll(".issue a");
 
-  console.log(issueLink);
   for (let i = 0; i < issueLink.length; i++) {
     issueLink[i].addEventListener("click", function(e) {
       e.preventDefault();
-      console.log("!!");
       const details = e.target.parentElement.querySelector(".details");
 
       if (details.classList.contains("hidden")) {
