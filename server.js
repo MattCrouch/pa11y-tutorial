f5f549c33b1aa8ee3e3ad32e2f9908baa863bdb8
Matt Crouch <matt@mattcrouch.net>
2019-07-27 12:04:07 +0100

    Login -> Dashboard structure

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const app = express();
 const path = require("path");
 const port = 3000;
 
-app.use("/static", express.static("static"));
+app.use("/static", express.static(path.join(__dirname, "site", "static")));
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "site", "index.html"));
 });
 
-app.get("/dashboard", function(req, res) {
-  res.sendFile(path.join(__dirname, "site", "dashboard.html"));
+app.get("/login", function(req, res) {
+  res.sendFile(path.join(__dirname, "site", "login.html"));
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
