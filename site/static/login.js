1a86d70ab98a905267fef540452f35738b8c2165
Matt Crouch <matt@mattcrouch.net>
2019-07-28 15:32:08 +0100

    Feedback

--- site/static/login.js
+++ site/static/login.js
@@ -1,8 +1,25 @@
 (function() {
   const loginForm = document.querySelector(".login-form");
 
+  const showWarning = (visible = false) => {
+    const feedback = loginForm.querySelector(".feedback");
+
+    if (feedback) {
+      loginForm.removeChild(feedback);
+    }
+
+    if (visible) {
+      const div = document.createElement("div");
+      div.className = "feedback";
+      div.innerText = "Could not log in please try again";
+      loginForm.prepend(div);
+    }
+  };
+
   loginForm.addEventListener("submit", function(e) {
     e.preventDefault();
+
+    showWarning(false);
 
     fetch("/login", {
       body: new FormData(loginForm),
@@ -10,6 +27,21 @@
         Accept: "application/json"
       },
       method: "POST"
-    });
+    })
+      .then(function(resp) {
+        if (!resp.ok) {
+          throw Error(resp.statusText);
+        }
+        return resp;
+      })
+      .then(function() {
+        // Success
+        window.location.href = "/";
+      })
+      .catch(function() {
+        // Fail
+        loginForm.reset();
+        showWarning(true);
+      });
   });
 })();
