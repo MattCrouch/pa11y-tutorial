d3d4101f11e917758f7cc3f99418d4163ae956ce
Matt Crouch <matt@mattcrouch.net>
2019-07-31 22:10:30 +0100

    Add comments

--- pa11y-test.js
+++ pa11y-test.js
@@ -1,7 +1,9 @@
+// Import Pa11y tools and file system modules
 const fs = require("fs-extra");
-const html = require("pa11y-reporter-html");
 const pa11y = require("pa11y");
+const html = require("pa11y-reporter-html");
 
+// Keep common default options separate
 const defaultOptions = {
   includeNotices: true,
   includeWarnings: true,
@@ -13,18 +15,24 @@ const defaultOptions = {
 };
 
 const pa11yTest = async (name, url, options = {}) => {
-  await fs.emptyDir(`results/${name}`);
+  // Make sure directory exists
+  await fs.ensureDir(`results/${name}`);
 
+  // Pa11y returns a Promise object
   return pa11y(url, {
     ...defaultOptions,
     ...options,
-    screenCapture: `results/${name}/screenshot.png`
+    screenCapture: `results/${name}/screenshot.png` // Save screenshots of every test
   })
     .then(async results => {
+      // Convert test result to HTML
       const htmlResult = await html.results(results);
+
+      // Output HTML to a `results` directory
       await fs.outputFile(`results/${name}/index.html`, htmlResult);
     })
     .catch(e => {
+      // Log any set up issues to investigate
       console.log(e);
     });
 };
--- tests.js
+++ tests.js
@@ -1,15 +1,23 @@
+// Import file system and pa11y-test modules
 const fs = require("fs-extra");
 const pa11yTest = require("./pa11y-test");
 
+// Pa11y runs asynchronously
 const runTests = async () => {
+  // Clear any previous results out
   await fs.emptyDir("results");
 
+  // Test the login page
   await pa11yTest("login", "localhost:3000/login");
+
+  // Test the initial dashboard view
   await pa11yTest("dashboard", "localhost:3000", {
     headers: {
       Cookie: "is-logged-in=1"
     }
   });
+
+  // Test what the page looks like when an issue is present
   await pa11yTest("more-info", "localhost:3000", {
     actions: ["click element .issue a"],
     headers: {
